f55ff78e11d459dd504a0bfedfce49fd3b6ef29c
Russiee <nikitavorontsov@hotmail.com>
2020-07-23 15:32:01 +0100

    Updated token so as not to be readable

--- src/main/java/com/discordbot/dnd/DnDDiscordBotApplication.java
+++ src/main/java/com/discordbot/dnd/DnDDiscordBotApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class DnDDiscordBotApplication {
@@ -21,11 +22,15 @@ public class DnDDiscordBotApplication {
 	@Autowired
 	private SummaryListener summaryListener;
 
+	@Autowired
+	private Environment env;
+
 	@Bean
 	@ConfigurationProperties("discord-api")
 	public DiscordApi discordApi() {
 		System.out.println("Initialising api");
-		DiscordApiBuilder builder = new DiscordApiBuilder().setToken("NzM1ODUyNDA4NTYzMDQwMzM2.XxmUgw.9BndQYp6vMLL21ua1poSwg6Xj1s");
+		String token = env.getProperty("TOKEN");
+		DiscordApiBuilder builder = new DiscordApiBuilder().setToken(token);
 		DiscordApi api = builder.login().join();
 		api.addListener(summaryListener);
 		return api;
