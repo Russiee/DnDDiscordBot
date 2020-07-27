65cb73e344b23aaae3ab6980934e0f179bf7c2b1
Russiee <nikitavorontsov@hotmail.com>
2020-07-27 11:01:20 +0100

    handout-listener: added enable jpa repository scan to autowire

--- src/main/java/com/discordbot/dnd/DnDDiscordBotApplication.java
+++ src/main/java/com/discordbot/dnd/DnDDiscordBotApplication.java
@@ -9,8 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class DnDDiscordBotApplication {
 
 	public static void main(String[] args) {
